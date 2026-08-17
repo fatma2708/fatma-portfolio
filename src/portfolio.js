f39c1ac8bedbfc5dd15e0795d140ba31f607d080
Fatma Ben Mlouka <fatmabenmlouka38@gmail.com>
2026-08-17 02:00:34 +0200

    fix: update contact email to Fatma's address

--- src/portfolio.js
+++ src/portfolio.js
@@ -372,8 +372,8 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+  number: "",
+  email_address: "fatmabenmlouka38@gmail.com"
 };
 
 // Twitter Section
